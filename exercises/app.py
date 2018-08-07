5437418291437393222a194c5e67bd32c59dedef
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:10:06 +0300

    doing more things

--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template(
-	"index.html")
+	likes_same_sport = True 
+	players = ["Jenny","Aya","Asaf"]
+	return render_template("index.html",players = players, likes_same_sport = likes_same_sport)
+
+
+
+
+
 
 
 
